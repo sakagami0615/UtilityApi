a5e6cfb8ab604a124a047c61330bef744536e259
sakagami0615 <sakagami0615@gmail.com>
2020-11-17 01:46:14 +0900

    add: chrome driver用のクラスを作成

---
+++ Python/selenium/chromedriver.py
@@ -0,0 +1,45 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+
+
+class ChromeDriver:
+	"""
+	ChromeのWebドライバ用のクラス
+
+	Attributes
+	----------
+	driver : selenium.webdriver.chrome.webdriver.WebDriver
+		ChromeのWebドライバ
+	wait_sec : float
+		Sleep用待機時間(秒)
+	"""
+
+	def __init__(self, wait_sec=0.5, is_hidden=True):
+
+		if is_hidden:
+			chrome_options = Options()
+			chrome_options.add_argument('--headless')
+			chrome_options.add_argument('--no-sandbox')
+			chrome_options.add_argument('--disable-dev-shm-usage')
+			
+		else:
+			self.driver = webdriver.Chrome()
+		
+		self.driver = webdriver.Chrome(chrome_options=chrome_options)
+		self.__wait_sec = wait_sec
+
+
+	def __del__(self):
+		"""
+		ドライバをクローズする
+		"""
+		self.driver.quit()
+	
+	
+	def Sleep(self):
+		"""
+		あらかじめ指定した時間だけ待機する
+		"""
+		time.sleep(self.__wait_sec)
